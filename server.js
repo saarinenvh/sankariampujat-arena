5d91dd5f5ea086507ed61eb4d1d485c2742b9132
saarinenvh <ville.h.saarinen@aalto.fi>
2020-04-07 19:24:48 +0300

    Removed response to server from apistats

--- server.js
+++ server.js
@@ -234,19 +234,16 @@ app.post("/api/match/:id/map/:map/start", (req, res) => {
   state.status = "LIVE";
   Object.assign(req.body, state.data);
   console.log(state.status);
-  res.send(match);
 });
 
 app.post("/api/match/:id/map/:map/finish", (req, res) => {
   state.status = "FINISHED";
   Object.assign(req.body, state.data);
   console.log(state.status);
-  res.send(match);
 });
 
 app.post("/api/match/:id/map/:map/update", (req, res) => {
   Object.assign(req.body, state.data);
-  res.send(match);
 });
 
 function createTeamDTO(team) {
